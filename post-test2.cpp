1258be78aca7c0124e2b0daf7b5413b180a8986e
Fikri Al Hafizh <91872566+Hafizh0192@users.noreply.github.com>
2021-11-26 11:01:39 +0700

    Add files via upload

---
+++ post-test2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (){
+
+	int a;
+	cin>>a;
+	int b;
+	cin>>b;
+	int c;
+	cin>>c;
+	int x;
+	cin>>x;
+	
+	cout<<(a*pow(x,2)+(b*x)+c)<<endl;
+
+}
